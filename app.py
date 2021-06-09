26cb2b932b185e3cb51a870d8347b075fdc326d6
SrinijaKatakam <katakamsrinija@gmail.com>
2021-06-09 10:50:15 -0500

    update1

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
+import re
+from azure.storage.blob import BlobServiceClient
+import pandas as pd
+
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "Hello, Srinijaaa!"
+def index():
+    return render_template('index.html')
+
+@app.route("/page1")
+def page1():
+    return render_template('page1.html')
+
+@app.route("/page2")
+def page2():
+    return render_template('page2.html')
+
+if __name__=='__main__':
+    app.run()
+
